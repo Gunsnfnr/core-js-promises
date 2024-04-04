854c5d65f7246ba1c717775b242614a8b8b15d27
Gunsnfnr <a.j.p.010203@gmail.com>
2024-04-04 23:06:08 +0300

    feat: add solutions for getPromise and getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,11 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number >= 0) resolve(number);
+    reject(number);
+  });
 }
 
 /**
@@ -50,8 +53,9 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const result = Promise.race(promises);
+  return result;
 }
 
 /**
